31bfa3b53ba5c550b74c7ed5cfbb399098034e59
deshmukhmahima32 <deshmukhmahima40@gmail.com>
2024-08-06 14:12:11 +0530

    Add files via upload

---
+++ StringArr1element.java
@@ -0,0 +1,18 @@
+public class StringArr1element {
+    public static void main(String [] args) 
+    {
+        String arr[]={"apple","Banana","Papaya"};
+        //String  Firstele;
+         for(int i=0;i<arr.length; i++)
+        {
+           // Firstele=arr[i];
+
+        
+        System.out.println(arr[i]);
+        
+        }
+
+        
+    }
+    
+}
